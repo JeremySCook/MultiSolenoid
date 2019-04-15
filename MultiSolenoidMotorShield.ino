4a9c67c0d46650331f2c7439fa3e441966837264
Jeremy S Cook <jscook55@gmail.com>
2019-04-15 11:29:06 -0400

    Update MultiSolenoidMotorShield.ino

--- MultiSolenoidMotorShield.ino
+++ MultiSolenoidMotorShield.ino
@@ -4,32 +4,32 @@
 
 #include <AFMotor.h>
 
-AF_DCMotor motor2(2);
-AF_DCMotor motor3(3);
-AF_DCMotor motor4(4);
+AF_DCMotor solenoid2(2);
+AF_DCMotor solenoid3(3);
+AF_DCMotor solenoid4(4);
 
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
   Serial.println("Motor test!");
 
   // turn on motor
-  motor2.setSpeed(255);
-  motor3.setSpeed(255);
-  motor4.setSpeed(255);
+  solenoid2.setSpeed(255);
+  solenoid3.setSpeed(255);
+  solenoid4.setSpeed(255);
   
 }
 
 void loop() {
-  motor2.run(FORWARD);
+  solenoid2.run(BACKWARD);
   delay(75);
-  motor2.run(RELEASE);
+  solenoid2.run(RELEASE);
   //delay(75);
-  motor3.run(FORWARD);
+  solenoid3.run(FORWARD);
   delay(75);
-  motor3.run(RELEASE);
+  solenoid3.run(RELEASE);
   //delay(75);
-  motor4.run(FORWARD);
+  solenoid4.run(FORWARD);
   delay(75);
-  motor4.run(RELEASE);
+  solenoid4.run(RELEASE);
   delay(75);
 }
